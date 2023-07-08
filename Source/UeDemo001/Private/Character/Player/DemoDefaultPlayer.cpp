7f158f2171385b476107e93b2f0fb800c195fdf2
paladin2003 <paladin2003@outlook.com>
2023-07-08 21:08:37 +0800

    实现增强输入移动操作

--- Source/UeDemo001/Private/Character/Player/DemoDefaultPlayer.cpp
+++ Source/UeDemo001/Private/Character/Player/DemoDefaultPlayer.cpp
@@ -2,20 +2,24 @@
 
 
 #include "Character/Player/DemoDefaultPlayer.h"
-
+#include "EnhancedInputComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 ADemoDefaultPlayer::ADemoDefaultPlayer()
 {
+	//创建弹簧臂组件
 	CameraArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraArm"));
 	CameraArm->AttachToComponent(GetRootComponent(),FAttachmentTransformRules::KeepRelativeTransform);
 	CameraArm->TargetArmLength = 800.f;
 	CameraArm->SetRelativeRotation(FRotator(-35,0,0));
 	CameraArm->bDoCollisionTest = false;
 
+	//创建摄像机组件
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	Camera->AttachToComponent(CameraArm,FAttachmentTransformRules::KeepRelativeTransform);
 
+	//调整骨骼网格体方向及位置
 	if (USkeletalMeshComponent* SkeletaMesh = GetComponentByClass<USkeletalMeshComponent>())
 	{
 		SkeletaMesh->SetRelativeRotation(FRotator(0,-90,0));
@@ -24,6 +28,10 @@ ADemoDefaultPlayer::ADemoDefaultPlayer()
 			SkeletaMesh->SetRelativeLocation(FVector(0,0,0 - Capsule->GetScaledCapsuleHalfHeight()));	
 		}
 	}
+
+	//默认移动倍率，
+	MovementRate = 5.f;
+	// GetCharacterMovement()->Velocity
 }
 
 void ADemoDefaultPlayer::BeginPlay()
@@ -33,7 +41,34 @@ void ADemoDefaultPlayer::BeginPlay()
 	InitEnhancedInput();
 }
 
-void ADemoDefaultPlayer::InitEnhancedInput()
+void ADemoDefaultPlayer::Move()
 {
 	
+}
+
+void ADemoDefaultPlayer::InitEnhancedInput()
+{
+	if(nullptr == MovementInputMappingContext)
+	{
+		return;
+	}
+	if (APlayerController* PlayerController = CastChecked<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(MovementInputMappingContext,0);
+		}
+		if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent))
+		{
+			EnhancedInputComponent->BindAction(MovementInputAction,ETriggerEvent::Triggered,this,&ADemoDefaultPlayer::MovementForEnhancedInput);
+		}
+	}
+}
+
+void ADemoDefaultPlayer::MovementForEnhancedInput(const FInputActionValue& InputActionValue)
+{
+	const FVector2d MovementValue = InputActionValue.Get<FVector2d>();
+
+	AddMovementInput(GetActorForwardVector() * MovementValue.X,MovementRate);
+	AddMovementInput(GetActorRightVector() * MovementValue.Y,MovementRate);
 }
--- Source/UeDemo001/Public/Character/Player/DemoDefaultPlayer.h
+++ Source/UeDemo001/Public/Character/Player/DemoDefaultPlayer.h
@@ -6,6 +6,8 @@
 #include "Camera/CameraComponent.h"
 #include "Character/DemoBaseCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
 #include "DemoDefaultPlayer.generated.h"
 
 /**
@@ -19,6 +21,11 @@ class UEDEMO001_API ADemoDefaultPlayer : public ADemoBaseCharacter
 	ADemoDefaultPlayer();
 
 protected:
+	/**
+	 * @brief 移动速率
+	 */
+	UPROPERTY(BlueprintReadWrite,EditAnywhere,Category="Movement")
+	float MovementRate;
 
 	UPROPERTY(BlueprintReadWrite,EditAnywhere)
 	USpringArmComponent* CameraArm;
@@ -26,12 +33,27 @@ protected:
 	UPROPERTY(BlueprintReadWrite,EditAnywhere)
 	UCameraComponent* Camera;
 
-	virtual void BeginPlay() override;
-	
+	/**
+	 * @brief 移动场景映射
+	 */
+	UPROPERTY(BlueprintReadWrite,EditAnywhere,Category="Enhanced Input")
+	TObjectPtr<UInputMappingContext> MovementInputMappingContext;
 
+	/**
+	 * @brief 移动动作
+	 */
+	UPROPERTY(BlueprintReadWrite,EditAnywhere,Category="Enhanced Input")
+	TObjectPtr<UInputAction> MovementInputAction;
+
+	virtual void BeginPlay() override;
+
+	virtual void Move();
+	
 private:
 	/**
 	 * @brief 开启增强输入
 	 */
 	void InitEnhancedInput();
+
+	virtual void MovementForEnhancedInput(const FInputActionValue& InputActionValue);
 };
